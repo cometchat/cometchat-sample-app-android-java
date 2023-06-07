ec701246240b2ac0c04620e8c4d4eb550b502cc3
vivekCometChat <vivek.prajapati@cometchat.com>
2023-06-07 16:56:58 +0530

    branch clean up

--- uikit/src/main/java/com/cometchat/pro/uikit/ui_resources/utils/custom_alertDialog/OnAlertDialogButtonClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.cometchat.pro.uikit.ui_resources.utils.custom_alertDialog;
-
-import android.view.View;
-
-import androidx.appcompat.app.AlertDialog;
-
-public interface OnAlertDialogButtonClickListener {
-	void onButtonClick(AlertDialog alertDialog, View v, int which, int popupId);
-}
--- uikit/src/main/java/com/cometchat/pro/uikit/ui_resources/utils/keyboard_utils/KeyboardVisibilityListener.java
+++
@@ -1,7 +0,0 @@
-package com.cometchat.pro.uikit.ui_resources.utils.keyboard_utils;
-
-public interface KeyboardVisibilityListener {
-
-    void onKeyboardVisibilityChanged(boolean keyboardVisible);
-
-}
