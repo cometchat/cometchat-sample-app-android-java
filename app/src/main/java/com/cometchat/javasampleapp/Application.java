59c3071bc744ce66c830b7855ac40f86233ac318
vivekCometChat <vivek.prajapati@cometchat.com>
2023-09-11 17:06:39 +0530

    v4.0.2

--- app/src/main/java/com/cometchat/javasampleapp/Application.java
+++ app/src/main/java/com/cometchat/javasampleapp/Application.java
@@ -1,23 +1,67 @@
 package com.cometchat.javasampleapp;
 
+import android.content.Context;
 import android.content.res.Configuration;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.cometchat.chat.core.Call;
+import com.cometchat.chatuikit.calls.CometChatCallActivity;
+import com.cometchat.chatuikit.shared.resources.theme.CometChatTheme;
+import com.cometchat.chatuikit.shared.resources.theme.Palette;
 import com.cometchat.chatuikit.shared.resources.utils.Utils;
 import com.cometchat.chat.core.CometChat;
 
 
 public class Application extends android.app.Application {
-    private static final String TAG = "UIKitApplication";
+    private static String LISTENER_ID;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        LISTENER_ID = System.currentTimeMillis() + "";
+        if(AppUtils.isNightMode(this)){
+            Palette.getInstance(this).mode(CometChatTheme.MODE.DARK);
+        }
     }
+
+    public static void addCallListener(Context context) {
+        CometChat.addCallListener(LISTENER_ID, new CometChat.CallListener() {
+            @Override
+            public void onIncomingCallReceived(Call call) {
+                CometChatCallActivity.launchIncomingCallScreen(context, call, null);
+            }
+
+            @Override
+            public void onOutgoingCallAccepted(Call call) {
+
+            }
+
+            @Override
+            public void onOutgoingCallRejected(Call call) {
+
+            }
+
+            @Override
+            public void onIncomingCallCancelled(Call call) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if(AppUtils.isNightMode(this)){
+            Palette.getInstance(this).mode(CometChatTheme.MODE.DARK);
+        }else{
+            Palette.getInstance(this).mode(CometChatTheme.MODE.LIGHT);
+        }
+    }
+
     @Override
     public void onTerminate() {
         super.onTerminate();
-        CometChat.removeConnectionListener(TAG);
     }
 }
